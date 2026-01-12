55441167bd7a99f66e072656b76cf71d11dd4520
Addishiwot Ali <sosenamillion@yahoo.com>
2026-01-11 20:51:42 -0800

    created area of acircle method

---
+++ Module1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Module1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         static double AreaOfACircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+
+        Console.WriteLine(AreaOfACircle(5));
+
+    }
+}
