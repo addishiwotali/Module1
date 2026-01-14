cf5c33e72ad0ccd513ccc24551456a179f7bc0e5
Addishiwot Ali <sosenamillion@yahoo.com>
2026-01-13 21:48:33 -0800

    userinput and called the method

--- Module1/Program.cs
+++ Module1/Program.cs
@@ -4,41 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-         static double AreaOfACircle(double radius)
+        static double AreaOfACircle(double radius)
         {
             var area = Math.PI * Math.Pow(radius, 2);
             return area;
         }
 
-        Console.WriteLine(AreaOfACircle(5));
+        Console.WriteLine("Enter radius of a circle:");
+        double radius = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Area of a circle is {AreaOfACircle(radius)}");
+
+        Console.WriteLine("-------------------------");
+        
+        
 
         static double AreaOfTriangle(double baseLength, double height)
         {
-            var bH =  baseLength * height;
+            var bH = baseLength * height;
             var area = bH / 2;
             return area;
         }
-        Console.WriteLine(AreaOfTriangle(10,5));
 
+        Console.WriteLine("Enter height of a triangle:");
 
+        double height = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Enter length of a triangle:");
+        double baseLength = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine($"Area of triangle is {AreaOfTriangle(baseLength, height)}");
+        
 
+       Console.WriteLine("--------------------------");
 
         static double AreaOfRectangle(double width, double height)
         {
             var area = width * height;
             return area;
         }
-        Console.WriteLine(AreaOfRectangle(10,5));
 
+        Console.WriteLine($"Enter width of a rectangle:");
+        
+        double width = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"Enter height of an rectangle:");
+        
+        double lenght = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine($"Area of rectangle is {AreaOfRectangle(width,height)}");
+        
+        
+        Console.WriteLine("--------------------------");
 
-
+        
         static double AreaOfSquare(double side)
         {
             var area = side * side;
             return area;
         }
-        Console.WriteLine(AreaOfSquare(10));
-    } 
-    
+
+        Console.WriteLine($"Enter width of a square:");
+        
+        double side = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine($"Area of square is {AreaOfSquare(side)}");
+        
+    }
 }
 
--- Module1/obj/Debug/net9.0/Module1.AssemblyInfo.cs
+++ Module1/obj/Debug/net9.0/Module1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4dd450ca824a1c9b725bf4506c7473d05ab243")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172ded9616403ccbe350940329747a77744be8ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
